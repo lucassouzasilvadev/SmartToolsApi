643dd51775df8694f874f1328619cedebe4c7dc8
lucassouza849 <lucas.sdsilva@bandtec.com.br>
2022-03-21 22:24:53 -0300

    subindo estrutura inicial do projeto

---
+++ mvp smart tools/mvp-smart-tools/src/main/java/smart/tools/api/mvp/smart/tools/MvpSmartToolsApplication.java
@@ -0,0 +1,13 @@
+package smart.tools.api.mvp.smart.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpSmartToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpSmartToolsApplication.class, args);
+	}
+
+}
---
+++ mvp smart tools/mvp-smart-tools/src/test/java/smart/tools/api/mvp/smart/tools/MvpSmartToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package smart.tools.api.mvp.smart.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpSmartToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
