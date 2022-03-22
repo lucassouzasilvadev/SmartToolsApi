5c6023ed20a51b2a7bd0b9384d74d615db3d6fc9
lucassouza849 <lucas.sdsilva@bandtec.com.br>
2022-03-21 23:17:31 -0300

    FEAT(ClienteRepository.java): criando repositorio cliente

--- mvp smart tools/mvp-smart-tools/src/main/java/smart/tools/api/mvp/smart/tools/repository/ClienteRepository.java
+++ mvp smart tools/mvp-smart-tools/src/main/java/smart/tools/api/mvp/smart/tools/repository/ClienteRepository.java
@@ -1,2 +1,12 @@
-package smart.tools.api.mvp.smart.tools.repository;public interface ClienteRepository {
+package smart.tools.api.mvp.smart.tools.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import smart.tools.api.mvp.smart.tools.model.Cliente;
+
+import java.util.Optional;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+    Optional<Cliente> findByEmail(String email);
 }
