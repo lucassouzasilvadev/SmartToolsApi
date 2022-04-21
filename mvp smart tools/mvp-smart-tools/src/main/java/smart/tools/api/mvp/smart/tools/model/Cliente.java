f33f51b4ed5847c08071d00c509b63e5507ea19f
Pedro Gimenez <pedro.msilva@bandtec.com.br>
2022-04-21 15:24:17 -0300

    modified backend

--- mvp smart tools/mvp-smart-tools/src/main/java/smart/tools/api/mvp/smart/tools/model/Cliente.java
+++ mvp smart tools/mvp-smart-tools/src/main/java/smart/tools/api/mvp/smart/tools/model/Cliente.java
@@ -35,4 +35,51 @@ public class Cliente {
     private String email;
 
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
